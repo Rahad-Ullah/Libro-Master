580fbb3cb81de75a8ee895a4857358518c736c3e
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-13 20:35:39 +0600

    Validation request body implemented

--- src/app/modules/Book/book.route.ts
+++ src/app/modules/Book/book.route.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { bookControllers } from "./book.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { bookValidationSchemas } from "./book.validation";
 const router = express.Router();
 
-router.post("/", bookControllers.createBook);
+router.post(
+  "/",
+  validateRequest(bookValidationSchemas.createValidationSchema),
+  bookControllers.createBook
+);
 
 export const bookRoutes = router;
