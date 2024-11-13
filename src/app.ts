d36fa308844a42b95e16479a36707c8486d970f7
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-13 18:24:16 +0600

    Routes setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -10,6 +11,9 @@ app.use(cookieParser());
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routes
+// app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
