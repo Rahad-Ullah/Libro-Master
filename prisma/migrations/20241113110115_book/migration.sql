bf9168b4f3e647525e6b1503804872998b9ee056
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-13 17:05:42 +0600

    Defined member model

---
+++ prisma/migrations/20241113110115_book/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "bookId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "totalCopies" INTEGER NOT NULL,
+    "availableCopies" INTEGER NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("bookId")
+);
