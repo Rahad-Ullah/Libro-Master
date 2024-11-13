d359d29a3fd6d14e4fcfe1f9f38b5a202afb22dd
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-13 20:38:47 +0600

    Global Error handler implemented

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -14,6 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api", router);
+
+// global error handler
+app.use(globalErrorHandler);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
